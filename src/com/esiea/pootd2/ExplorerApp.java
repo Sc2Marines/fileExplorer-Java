b1266dd2d4d12d1df68c54fe978c2f86417a28fc
Sc2Marines <alexandre.foll35@gmail.com>
2024-12-07 18:50:35 +0100

    feat: :sparkles: text interface working
    remove all test shit, add initialisation into textInterface
    
    none

--- src/com/esiea/pootd2/ExplorerApp.java
+++ src/com/esiea/pootd2/ExplorerApp.java
@@ -13,23 +13,8 @@ public class ExplorerApp {
 
         ExplorerController controller = new ExplorerController();
         controller.initialiseBaseArchitecture();
-        System.out.println(controller.executeCommand("touch test.c"));
-        System.out.println(controller.executeCommand("ls"));
-        System.out.println(controller.executeCommand("cd /POO/TD2"));
-        System.out.println(controller.executeCommand("mkdir tmp"));
-        System.out.println(controller.executeCommand("mkdir /POO/TD2/tmp/tmp2"));
-        System.out.println(controller.executeCommand("touch /POO/TD2/tmp/test2.c"));
-        System.out.println(controller.executeCommand("touch /POO/TD2/tmp/tmp2/test3.c"));
-        System.out.println(controller.executeCommand("ls /"));
-        System.out.println(controller.executeCommand("ls ../"));
-        System.out.println(controller.executeCommand("mkdir /POO/TD2/tmp/tmp2/../tmp3"));
-        System.out.println(controller.executeCommand("touch /POO/TD2/tmp/tmp2/../tmp3/test4.c"));
-        System.out.println(controller.executeCommand("touch /POO/TD2/tmp/tmp2/../tmp3/test4.c"));
-        System.out.println(controller.executeCommand("cd ./tmp/tmp2/../"));
-        System.out.println(controller.executeCommand("cd /"));
-        System.out.println(controller.executeCommand("tree"));
         
-
         TextInterface textInterface = new TextInterface(controller);
+        textInterface.run();
     }
 }
